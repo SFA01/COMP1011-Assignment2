26bfd9b2d0aa1a9df7569e009d9418bdecbff1a6
SFA01 <sarafranzinoandrade@hotmail.com>
2022-07-03 18:01:16 -0400

    Scenes added

--- src/main/java/ca/georgiancollege/comp1011assignment2/Scene2Controller.java
+++ src/main/java/ca/georgiancollege/comp1011assignment2/Scene2Controller.java
@@ -1,2 +1,38 @@
-package ca.georgiancollege.comp1011assignment2;public class Scene2Controller {
+package ca.georgiancollege.comp1011assignment2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class Scene2Controller {
+
+    @FXML
+    private TableColumn<?, ?> Code;
+
+    @FXML
+    private TableColumn<?, ?> Continent;
+
+    @FXML
+    private TableColumn<?, ?> Name;
+
+    @FXML
+    private Button OnClickedButton2;
+
+    @FXML
+    private TableColumn<?, ?> Population;
+
+    @FXML
+    private TableColumn<?, ?> Region;
+
+    @FXML
+    private TableView<?> Table;
+
+    @FXML
+    void OnClickedButton2(ActionEvent event) {
+
+    }
+
 }
+
